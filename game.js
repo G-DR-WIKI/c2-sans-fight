b58da1c7daf23d71e63f477875656a73bb2f10e5
Gil <g.dr.wiki@proton.me>
2026-04-01 03:44:07 +0200

    Update data.js

---
+++ game.js
@@ -0,0 +1,32 @@
+window.GS_A = true;
+window.GS_B = false;
+window.GS_C = false;
+
+setInterval(() => {
+	const runtime = cr_getC2Runtime();
+	if (!runtime) {
+		console.log("No runtime available");
+		return;
+	}
+
+	const HP = runtime.Gu?.find(a => a.name === "HP");
+	if (!HP) {
+		console.log("No HP available");
+		return;
+	}
+
+	const currScene = runtime.ba?.name;
+
+	if (currScene === "BattleScreen") {
+		window.GS_B = true;
+
+		if (HP.data <= 0) {
+			console.log("GAME LOST");
+			window.parent.postMessage({ evt: 'loss' });
+			window.GS_A = false;
+		}
+	} else if (currScene === "MainMenu" && window.GS_B) {
+		console.log("GAME WON");
+		window.parent.postMessage({ evt: 'win' });
+	}
+}, 500);
